f937d5ac307ee4d2f500aae694311307cdb89f66
mehboobpatel <92875632+mehboobpatel@users.noreply.github.com>
2023-02-14 14:40:10 +0530

    Update service.php

--- service.php
+++ service.php
@@ -11,30 +11,30 @@ include ('./include/header.php')
         <div class="container">
             <div class="wrapper d-flex flex-column gap-4">
                 <div class="element ev">
-                    <h3><a href="events" class="text-decoration-none text-white">Events<i class="fa-solid ms-3 fa-chevron-up"></i></a></h3>
+                    <h3><a href="events.php" class="text-decoration-none text-white">Events<i class="fa-solid ms-3 fa-chevron-up"></i></a></h3>
                     <p>The landscape of events have changed over few years . We have been progressive to adapt quickly. We integrate technology with stories and emotions to provide world class experience. </p>
                 </div>
                 <div class="element vd">
-                    <h3><a href="video" class="text-decoration-none text-white">Video Production<i class="fa-solid ms-3 fa-chevron-up"></i></a></h3>
+                    <h3><a href="video.php" class="text-decoration-none text-white">Video Production<i class="fa-solid ms-3 fa-chevron-up"></i></a></h3>
                     <p>Most brands are driven by SEO tactics and have almost forgotten about creating a personal experience with their customers.  Coming up with the best experiential marketing strategies is the first step towards creating an amazing experience for you and your customers.
 </p>
                 </div>
                 <div class="element cc">
-                    <h3><a href="creative" class="text-decoration-none text-white">Creative and Communication<i class="fa-solid ms-3 fa-chevron-up"></i></a></h3>
+                    <h3><a href="creative.php" class="text-decoration-none text-white">Creative and Communication<i class="fa-solid ms-3 fa-chevron-up"></i></a></h3>
                     <p>Making your content and marketing campaigns more visual, interactive, and gamified you're much more likely to engage your subscribers and consumer. <br>
                     Our SME's read your customer mind and heart to create a personalized customer experience through content and creative.
 
 </p>
                 </div>
                 <div class="element ma">
-                    <h3><a href="marketing" class="text-decoration-none text-white">Marketing Analytics<i class="fa-solid ms-3 fa-chevron-up"></i></a></h3>
+                    <h3><a href="marketing.php" class="text-decoration-none text-white">Marketing Analytics<i class="fa-solid ms-3 fa-chevron-up"></i></a></h3>
                     <p>To make experience measurable we provide performance support in all facets of your campaign be it offline or online
 Improve your roi with scalable ppc marketing.
 </p>
                 </div>
                 
                     <div class="element im">
-                    <h3><a href="image" class="text-decoration-none text-white">Image Management<i class="fa-solid ms-3 fa-chevron-up"></i></a></h3>
+                    <h3><a href="image.php" class="text-decoration-none text-white">Image Management<i class="fa-solid ms-3 fa-chevron-up"></i></a></h3>
                     <p>As said by Kotler "An image is the set of beliefs, ideas, and impression that a person holds regarding an object"  we make sure that your impression is being delievered to the best by following "Panchatv" (the five aspects of truth).<p>
                 </div>
                 
